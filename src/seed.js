18649d21b42b37dabfb24ad31b8c0426d225f49e
Hani <hanider27@gmail.com>
2026-01-29 20:19:57 +0100

    add seed.js for ading products

---
+++ src/seed.js
@@ -0,0 +1,42 @@
+import pkg from 'pg';
+const { Pool } = pkg;
+import dotenv from 'dotenv';
+import { join } from 'path';
+
+dotenv.config({ path: join(process.cwd(), '..', '.env') });
+
+const pool = new Pool({
+    user: process.env.POSTGRES_USER || 'postgres',
+    host: 'localhost',
+    database: process.env.POSTGRES_DB || 'postgres',
+    password: process.env.POSTGRES_PASSWORD || '19000',
+    port: process.env.POSTGRES_PORT || 5432,
+});
+
+const seedDatabase = async () => {
+    try {
+        console.log("🚀 Connexion à PostgreSQL...");
+
+        // Nettoyage
+        await pool.query('TRUNCATE TABLE products CASCADE');
+
+        // On ajoute NOW() pour les colonnes createdAt et updatedAt
+        const queryText = `
+      INSERT INTO products (id, name, categorie, prix, description, image_url, "createdAt", "updatedAt") VALUES 
+      (gen_random_uuid(), 'Burger Signature', 'Burger', 12.00, 'Le classique de la maison', '/images/card1.png', NOW(), NOW()),
+      (gen_random_uuid(), 'Frites Maison', 'Frites', 4.50, 'Pommes de terre fraîches', '/images/card2.png', NOW(), NOW()),
+      (gen_random_uuid(), 'Wings de Poulet', 'Poulets', 10.00, 'Ailes croustillantes', '/images/card3.png', NOW(), NOW()),
+      (gen_random_uuid(), 'Coca-Cola', 'Boissons', 2.50, 'Canette 33cl', '/images/card4.png', NOW(), NOW())
+    `;
+
+        await pool.query(queryText);
+        console.log("✅ SEED RÉUSSI ! Tes 4 produits sont créés avec leurs images.");
+
+    } catch (err) {
+        console.error("❌ Erreur pendant le seed :", err);
+    } finally {
+        await pool.end();
+    }
+};
+
+seedDatabase();
